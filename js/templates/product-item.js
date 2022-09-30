f643bac06fb530fb328dde9bc71d6fd78b4b48d6
Yana <yanaegorova69.3@gmail.com>
2022-09-30 18:39:22 +0300

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -19,9 +19,10 @@ export const item = (product) => {
       $${(product.price).toFixed(2)}
       </span>
       <div class="product__btn-block">
-      <a href="product-page.html?id=${product.id}" class="btn btn--first">details</a>
-  
-    <button class="btn js_add-to-cart">buy</button>
+      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details</a>
+      ${
+        product.isItRing ? '' : '<button class="btn js_add-to-cart">buy</button>'
+      }
     </div>
       </div>
 
